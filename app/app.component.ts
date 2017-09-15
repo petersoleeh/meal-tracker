fbe1f698c1b23cce78a36e06836c3d69d029dd7e
peter soleeh <petersoleeh@gmail.com>
2017-09-15 13:47:14 +0300

    app structure

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="jumbotron">
+    <h1>The  Meal Tracker!</h1>
+  </div>
+  `
+})
+
+export class AppComponent { }
