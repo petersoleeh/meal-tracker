ad672e15785c961778ac5afd94022cca55e07d5e
peter soleeh <petersoleeh@gmail.com>
2017-09-15 15:16:31 +0300

    add ngModel this will allow users to change the name of the food live on the html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Food } from './food.model';
 @Component({
   selector: 'my-app',
   template: `
   <div class="jumbotron">
     <h1>The  Meal Tracker!</h1>
   </div>
+  <h2>{{Food.Name}} Details</h2>
+  <div><label>Calories:</label><h3>{{Food.calories}}</h3>
+  <label>Details:</label><h4>{{Food.details}}</h4></div>
+  <div>
+    <label>Food Name: </label>
+    <input [(ngModel)]="Food.Name" placeholder="food name">
+  </div>
   `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  public Food = {
+    Name: "Hamberger",
+    calories: 25325,
+    details: "greasy",
+
+  }
+
+
+}
