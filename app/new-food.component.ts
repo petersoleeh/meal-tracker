0526ef8ec53b509bbc08ad12371b9fd533c55823
peter soleeh <petersoleeh@gmail.com>
2017-10-01 09:01:52 +0300

    new food form

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -4,10 +4,29 @@ import { Food } from './food.model';
 @Component({
   selector: 'new-food',
   template: `
-    <h3>Chill</h3>
+    <h3>Add Food</h3>
+    <div>
+      <label>Food:</label>
+      <input #newFood>
+    </div>
+    <div>
+      <label>Calories:</label>
+      <input #newCalories>
+    </div>
+    <div>
+      <label>Details:</label>
+      <input #newDetails>
+      <button (click)="addClicked(newFood.value, newCalories.value, newDetails.value)">Add</button>
+    </div>
+
   `
 })
 
 export class NewFoodComponent {
+  @Output() newFoodSender = new EventEmitter();
+  addClicked(food: string, calories: number, details: string) {
+    var newFoodToAdd: Food = new Food(food, calories, details);
+    this.newFoodSender.emit(newFoodToAdd);
+  }
 
 }
